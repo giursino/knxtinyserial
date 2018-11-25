1b3ab5cf4d73f2e69a6211505661615aec9f9697
Giuseppe Ursino <giursino@gmail.com>
2018-11-25 22:48:01 +0100

    created toolchain for RPI with GCC 6.3.1

--- CMakeToolchainFile-RaspberryGCC6.cmake
+++ CMakeToolchainFile-RaspberryGCC6.cmake
@@ -2,20 +2,33 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
  
 # Specify the sdk path
-set(SDK_PATH /home/giursino/Scaricati/rpi-newer-crosstools-master/x64-gcc-6.3.1/arm-rpi-linux-gnueabihf)
+set(SDK_PATH /opt/rpi-newer-crosstools-master/x64-gcc-6.3.1/arm-rpi-linux-gnueabihf)
 
 # specify the cross compiler
 set(CMAKE_C_COMPILER   "${SDK_PATH}/bin/arm-rpi-linux-gnueabihf-gcc")
 set(CMAKE_CXX_COMPILER "${SDK_PATH}/bin/arm-rpi-linux-gnueabihf-g++")
 
+# Specify the system sysroot
+set(CMAKE_SYSROOT "/mnt/ggrasp")
+
 # Specify flags
-set(COMMON_FLAGS "-march=armv6z -mtune=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard")
+# I need to specify -I, -B and -rpath because the toolchain was build for TARGET arm-rpi-linux-gnueabihf
+# (see $CMAKE_CXX_COMPILER -dumpmachine) while the sysroot was created (by raspbian) for target
+# arm-linux-gnueabihf. So paths of libc and libraries doesn't match.
+set(COMMON_FLAGS " \
+    -march=armv6z -mtune=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard \
+    -I${CMAKE_SYSROOT}/usr/include/arm-linux-gnueabihf \
+    -B${CMAKE_SYSROOT}/lib/arm-linux-gnueabihf \
+    -B${CMAKE_SYSROOT}/usr/lib/arm-linux-gnueabihf \
+    -Wl,-rpath-link,${CMAKE_SYSROOT}/lib/arm-linux-gnueabihf \
+    -Wl,-rpath-link,${CMAKE_SYSROOT}/usr/lib/arm-linux-gnueabihf \
+    ")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${COMMON_FLAGS}" CACHE STRING "")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COMMON_FLAGS}" CACHE STRING "")
  
-# Specify the system sysroot
-#set(CMAKE_SYSROOT "/home/giursino/Scaricati/sysroot-glibc-linaro-2.23-2017.05-arm-linux-gnueabihf")
- 
+# where is the target environment
+SET(CMAKE_FIND_ROOT_PATH ${CMAKE_SYSROOT})
+
 # search for programs in the build host directories
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
  
