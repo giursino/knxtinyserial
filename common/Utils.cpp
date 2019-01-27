cac141d320335e7c38aedc7b1ebda654c4a97401
Giuseppe Ursino <giursino@gmail.com>
2019-01-27 16:52:36 +0100

    fix author name

--- common/Utils.cpp
+++ common/Utils.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 giursino
+Copyright 2019 Giuseppe Ursino
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
